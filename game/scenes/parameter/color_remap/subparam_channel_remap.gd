fa2befcd3f5b9bb7803ead8592da97a0deb70474
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2025-04-30 23:05:24 -0500

    Fixup

--- game/scenes/parameter/color_remap/subparam_channel_remap.gd
+++ game/scenes/parameter/color_remap/subparam_channel_remap.gd
@@ -1,13 +1,3 @@
-@tool extends PanelContainer
+extends PanelContainer
 
-@export var enabled : bool :
-	get: return $settings/enabled.value if find_child("enabled") else false
-	set(val):
-		if not find_child("enabled"): return
-		$settings/enabled.value = val
 
-@export var suffix : String :
-	get: return $settings/suffix.value if find_child("suffix") else "_"
-	set(val):
-		if not find_child("suffix"): return
-		$settings/suffix.value = val
